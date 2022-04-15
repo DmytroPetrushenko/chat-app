b795b4ab14fd51f2a33869b58d8bdd849895d163
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2022-04-15 13:06:45 +0300

    models done

---
+++ src/main/java/trackensure/model/Messages.java
@@ -0,0 +1,40 @@
+package trackensure.model;
+
+public class Messages {
+    private Long id;
+    private Long userId;
+    private String message;
+    private String timeStamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
