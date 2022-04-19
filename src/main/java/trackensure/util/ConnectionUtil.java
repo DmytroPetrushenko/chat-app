0006cd4019e03a0ab44576f8083ab56dcfbf3cce
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2022-04-19 15:02:31 +0300

    db.properties done

--- src/main/java/trackensure/util/ConnectionUtil.java
+++ src/main/java/trackensure/util/ConnectionUtil.java
@@ -1,33 +1,39 @@
 package trackensure.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://http.localhost:3306/trackensure"
-            + "?serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1234";
+    private static final String DB_PROPERTIES = "db.properties";
 
-    static {
+    public static Connection getConnection() {
         try {
-            Class.forName(JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't get JDBC driver", e);
+            ConnectionUtil connectionUtil = new ConnectionUtil();
+            FileInputStream stream = new FileInputStream(connectionUtil.getFileFromResources());
+            Properties dbProperties = new Properties();
+            dbProperties.load(stream);
+
+            String url = dbProperties.getProperty("url");
+            Class.forName(dbProperties.getProperty("driver"));
+            return DriverManager.getConnection(url, dbProperties);
+        } catch (ClassNotFoundException | SQLException | IOException  e) {
+            throw new RuntimeException("Can't get a connection!", e);
         }
     }
 
-    public static Connection getConnection() {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
-        try {
-            return DriverManager.getConnection(URL, dbProperties);
-        } catch (SQLException e) {
-            throw new RuntimeException("Can't create a connection to DB", e);
+    private File getFileFromResources() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(DB_PROPERTIES);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
         }
     }
 }
